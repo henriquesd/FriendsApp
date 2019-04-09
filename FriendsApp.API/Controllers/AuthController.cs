4a84e0b72c36700e1a47f0e38ad80237f0ba713a
HenriqueSD <hsd.foz@gmail.com>
2019-04-09 20:15:09 -0300

    Created Login Method in the API and Fixed the location of the ConnectionStrings, changing to the appsettings.json file

--- FriendsApp.API/Controllers/AuthController.cs
+++ FriendsApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using FriendsApp.API.Data;
 using FriendsApp.API.Dtos;
 using FriendsApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FriendsApp.API.Controllers
 {
@@ -11,8 +17,10 @@ namespace FriendsApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repository;
-        public AuthController(IAuthRepository repository)
+        public IConfiguration _config { get; set; }
+        public AuthController(IAuthRepository repository, IConfiguration config)
         {
+            this._config = config;
             this._repository = repository;
         }
 
@@ -62,5 +70,40 @@ namespace FriendsApp.API.Controllers
         //     // TODO: change to return CreatedAtRoute (now don't have a method to currently get the user at this time);
         //     return StatusCode(201); // 201 is the state code of CreatedAtRoute;
         // }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+               token = tokenHandler.WriteToken(token) 
+            });
+        }
     }
 }
