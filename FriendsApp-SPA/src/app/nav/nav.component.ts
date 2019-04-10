184b879c1d8682b33ee891182d6489e1a3880b2d
HenriqueSD <hsd.foz@gmail.com>
2019-04-10 20:19:29 -0300

    Implemented login and logout options

--- FriendsApp-SPA/src/app/nav/nav.component.ts
+++ FriendsApp-SPA/src/app/nav/nav.component.ts
@@ -22,4 +22,15 @@ export class NavComponent implements OnInit {
     });
   }
 
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    // if there is something in this token then it's gonna return true, if this token variable is empty then it's gonna return false;
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('Logget out');
+  }
+
 }
