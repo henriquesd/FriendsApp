7da476b5cd26aec6a8e5bab181592ab96adb4b7d
HenriqueSD <hsd.foz@gmail.com>
2019-04-15 20:43:00 -0300

    Using Auth0 JwtModule to send up JWT tokens automatically

--- FriendsApp-SPA/src/app/_services/user.service.ts
+++ FriendsApp-SPA/src/app/_services/user.service.ts
@@ -4,13 +4,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 
-const httpOptions = {
-  // just temporary, after will be changed for a different way of sending token automatically to the server;
-  headers: new HttpHeaders({
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
-  })
-};
-
 @Injectable({
   providedIn: 'root'
 })
@@ -20,11 +13,11 @@ export class UserService {
 constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<User[]>(this.baseUrl + 'users');
   }
 
   getUser(id): Observable<User> {
-    return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 
 }
