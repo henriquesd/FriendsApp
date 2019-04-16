59a7bf3c1c5fe53892fad1fabc0b2af9b20c2588
HenriqueSD <hsd.foz@gmail.com>
2019-04-16 18:39:44 -0300

    Created MemberDetailComponent

---
+++ FriendsApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/user.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-member-detail',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+  user: User;
+
+  constructor(private userService: UserService, private alertify: AlertifyService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  // members/4
+  loadUser() {
+    // "+" it's used to force the "id" to be passed as a number, the id will be retrieved
+    // from the url as string but then converted to number;
+    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
+      console.log(user);
+      this.user = user;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
+
+}
