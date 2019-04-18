ac8ca8e827f18fa816855f9f7083f1f871361be3
HenriqueSD <hsd.foz@gmail.com>
2019-04-18 19:15:21 -0300

    Added the Set Main photo functionality to the API

--- FriendsApp.API/Data/IDatingRepository.cs
+++ FriendsApp.API/Data/IDatingRepository.cs
@@ -12,5 +12,6 @@ namespace FriendsApp.API.Data
          Task<IEnumerable<User>> GetUsers();
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
+         Task<Photo> GetMainPhotoForUser(int userId);
     }
 }
