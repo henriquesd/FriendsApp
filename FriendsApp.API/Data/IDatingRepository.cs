c1e7059d6eb77ebeabe7e2c766bc235f47a7b667
HenriqueSD <hsd.foz@gmail.com>
2019-04-18 17:34:46 -0300

    Implemented photo upload on API

--- FriendsApp.API/Data/IDatingRepository.cs
+++ FriendsApp.API/Data/IDatingRepository.cs
@@ -11,5 +11,6 @@ namespace FriendsApp.API.Data
          Task<bool> SaveAll();
          Task<IEnumerable<User>> GetUsers();
          Task<User> GetUser(int id);
+         Task<Photo> GetPhoto(int id);
     }
 }
