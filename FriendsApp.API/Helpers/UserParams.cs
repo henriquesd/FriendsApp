fdb0ea356ce4759972153268bec5a3c14c0a253a
henriquesd <hsd.foz@gmail.com>
2019-04-24 12:49:47 -0300

    Added filter by minAge and maxAge in the API

--- FriendsApp.API/Helpers/UserParams.cs
+++ FriendsApp.API/Helpers/UserParams.cs
@@ -13,5 +13,7 @@ namespace FriendsApp.API.Helpers
         
         public int UserId { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
