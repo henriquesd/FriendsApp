ff644c556470480eb73d560cc2e2be1fa8174410
henriquesd <hsd.foz@gmail.com>
2019-04-24 13:10:53 -0300

    Implemented sorting option in the API

--- FriendsApp.API/Data/DatingRepository.cs
+++ FriendsApp.API/Data/DatingRepository.cs
@@ -51,7 +51,8 @@ namespace FriendsApp.API.Data
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = _context.Users.Include(p => p.Photos).AsQueryable();
+            var users = _context.Users.Include(p => p.Photos)
+                .OrderByDescending(u => u.LastActive).AsQueryable();
 
             users = users.Where(u => u.Id != userParams.UserId);
 
@@ -63,6 +64,19 @@ namespace FriendsApp.API.Data
                 var maxDateOfBirth = DateTime.Today.AddYears(-userParams.MinAge);
 
                 users = users.Where(u => u.DateOfBirth >= minDateOfBirth && u.DateOfBirth <= maxDateOfBirth);
+            }
+
+            if (!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch (userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
